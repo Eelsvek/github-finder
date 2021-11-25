8d933399666b4dc906003244887c2a72bb91a96a
Kevs <kev.sunlee@gmail.com>
2021-11-25 08:19:12 -0800

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-import React, { Fragment, Component } from 'react';
+import React, { Component } from 'react';
+import NavBar from './components/layout/NavBar';
 import './App.css';
 
 class App extends Component {
   render() {
-    const name = 'Kevin';
-    const loading = false;
-    const showName = true;
-
     return (
-      <Fragment>
-        <div className='App'>
-          {loading ? <h4>Loading...</h4> : <h1>Hello {showName && name}</h1>}
-        </div>
-        <h2>Hmm</h2>
-      </Fragment>
+      <div className='App'>
+        <NavBar icon='fab fa-github' title='Github Finder' />
+      </div>
     );
   }
 }
