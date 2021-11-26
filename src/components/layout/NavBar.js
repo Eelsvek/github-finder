b144737bd7260f8b0947a0a6491cedf46e8e95d6
Kevs <kev.sunlee@gmail.com>
2021-11-25 23:22:43 -0800

    functional components

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,27 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-export class NavBar extends Component {
-  // static defaultProps = {
-  //   icon: 'fab fa-github',
-  //   title: 'Github Finder',
-  // };
+const NavBar = ({ icon, title }) => {
+  return (
+    <nav className='navbar bg-primary'>
+      <h1>
+        <i className={icon} />
+        {title}
+      </h1>
+    </nav>
+  );
+};
 
-  static propTypes = {
-    icon: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-  };
+NavBar.defaultProps = {
+  icon: 'fab fa-github',
+  title: 'Github Finder',
+};
 
-  render() {
-    return (
-      <nav className='navbar bg-primary'>
-        <h1>
-          <i className={this.props.icon} />
-          {this.props.title}
-        </h1>
-      </nav>
-    );
-  }
-}
+NavBar.propTypes = {
+  icon: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+};
 
 export default NavBar;
