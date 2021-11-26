2cfcab328d7f12ec68b71faf87949d79a35ce3a8
Kevs <kev.sunlee@gmail.com>
2021-11-26 12:08:21 -0800

    react-router-dom v6

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import './App.css';
+import axios from 'axios';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import NavBar from './components/layout/NavBar';
 import Alert from './components/layout/Alert';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
-import './App.css';
-import axios from 'axios';
+import About from './components/pages/About';
+import User from './components/users/User';
 
 class App extends Component {
   state = {
     users: [],
+    user: null,
     loading: false,
     alert: null,
   };
@@ -43,23 +48,52 @@ class App extends Component {
     }
   };
 
+  getUser = async (username) => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users/${username}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ user: res.data, loading: false });
+  };
+
   render() {
-    const { users, loading, alert } = this.state;
+    const { users, loading, alert, user } = this.state;
 
     return (
-      <div className='App'>
-        <NavBar icon='fab fa-github' title='Github Finder' />
-        <div className='container'>
-          <Alert alert={alert} />
-          <Search
-            clearUsers={this.clearUsers}
-            searchUsers={this.searchUsers}
-            setAlert={this.setAlert}
-            showClear={!!users.length}
-          />
-          <Users loading={loading} users={users} />
+      <Router>
+        <div className='App'>
+          <NavBar icon='fab fa-github' title='Github Finder' />
+          <div className='container'>
+            <Alert alert={alert} />
+            <Routes>
+              <Route
+                exact
+                path='/'
+                element={
+                  <Fragment>
+                    <Search
+                      clearUsers={this.clearUsers}
+                      searchUsers={this.searchUsers}
+                      setAlert={this.setAlert}
+                      showClear={!!users.length}
+                    />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                }
+              />
+              <Route exact path='/about' element={<About />} />
+              <Route
+                exact
+                path='/user/:login'
+                element={
+                  <User getUser={this.getUser} user={user} loading={loading} />
+                }
+              />
+            </Routes>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
