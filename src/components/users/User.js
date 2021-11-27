ae1fd8593edcb2eb7a33f3f428bb003307b36167
Kevs <kev.sunlee@gmail.com>
2021-11-27 11:37:26 -0800

    User component

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -3,16 +3,19 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import Spinner from '../layout/Spinner';
+import Repos from '../repos/Repos';
 
 class User extends Component {
   static propTypes = {
     getUser: PropTypes.func.isRequired,
     loading: PropTypes.bool.isRequired,
     user: PropTypes.object.isRequired,
+    repos: PropTypes.array.isRequired,
   };
 
   componentDidMount() {
     this.props.getUser(this.props.match.params.login);
+    this.props.getUserRepos(this.props.match.params.login);
   }
 
   render() {
@@ -31,7 +34,7 @@ class User extends Component {
       public_gists,
       name,
     } = this.props.user;
-    const { loading } = this.props;
+    const { loading, repos } = this.props;
 
     if (loading) {
       return <Spinner />;
@@ -102,6 +105,7 @@ class User extends Component {
           </div>
           <div className="badge badge-dark">Public Gists: {public_gists} </div>
         </div>
+        <Repos repos={repos} />
       </Fragment>
     );
   }
